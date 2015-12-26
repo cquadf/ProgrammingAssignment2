a33ef6bf016c3a3aa40f56c4b73da9a004af0d30
unknown <carsten@quadfliegen.de>
2015-12-26 21:40:27 +0100

    edit cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,71 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera - Data Science 2 - Prog. Assignment 2
+## Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix creates functions set, get, setinverse, getinverse and return as list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve delivers the inverse matrix. 
+## On the first run it goes the else path and calculates the inverse.
+## On subsequent runs the inverse is simply returned
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+#### Tests Matrix
+
+# ?solve()
+# v <- 1:4
+# v
+# mat <- matrix(v,2,2)
+# mat
+# imat <- solve(mat)
+# imat
+# t <- solve(mat) %*% mat
+# t
+# 
+# w <- 11:14
+# mat2 <- matrix(w,2,2)
+# mat2
+# imat2 <- solve(mat2)
+# imat2
+# u <- solve(mat2) %*% mat2
+# u
+# 
+# 
+# a <- makeCacheMatrix (mat)
+# a$get()
+# a$set()
+# a$getinverse()
+# a$setmean()
+# cacheSolve(a)
+# a$set(mat2)
+# 
+# 
+# 
+# 
+# 
